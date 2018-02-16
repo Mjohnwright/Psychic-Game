4dae983c53c75d9b37f920d83f6f5bbb4db8d0b4
Mjohnwright <34377327+Mjohnwright@users.noreply.github.com>
2018-02-16 15:20:50 -0500

    still working making random letter work properly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,48 +5,56 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
 wins = 0;
 losses = 0;
 guessesLeft = 10;
-
+guessesSoFar = 0;
 
 //Computer picks a letter and gets stored as computerChoice
 var computerChoice = function () {
 
-    // guessesSoFar = 0;
-    var computerCurrentChoice = letters[Math.floor(Math.random() * letters.length)]
-    console.log("computerChoice " + computerCurrentChoice);
+    // computer chooses a letter and it is assigned a variable name
+    var computerCurrentChoice = letters[Math.floor(Math.random() * letters.length)];
+    console.log("current Choice " + computerCurrentChoice);
+
+    // when User presses key it launches a function that stores the letter value
     document.onkeypress = function (event) {
         var userGuess = event.key;
         console.log("userGuess      " + userGuess);
 
 
-        // if the user and the computer choose the same letter: wins adds 1, alert is launched
+        // If the user and the computer choose the same letter: 
+        // wins adds 1, 
+        // guessesLeft and guessesSoFar are reset to 10 and 0 respectively, 
+        //alert is launched notifying user of win
         if (userGuess === computerCurrentChoice) {
             wins++;
-            computerChoice;
-            // guessesLeft = 10;
+            guessesLeft = 10;
+            guessesSoFar = 0;
             alert("You won!!!");
-            // console.log("computerChoice " + computercurrentChoice);
 
 
-            // if userGuess and computerChoice are different: guessesLeft decreases 1, guessesSoFar increases 1
+            // if userGuess and computerChoice are different: 
+            // guessesLeft decreases 1, 
+            // guessesSoFar increases 1
         } else {
             guessesLeft--;
-            console.log(computerChoice);
-            console.log("computerChoice " + computerCurrentChoice);
+            guessesSoFar++;
 
+            // if guesses left reached 0:
+            //alert is launched notifying user of loss
+            // guessesLeft and guessesSoFar are reset to 10 and 0 respectively,
+            // losses adds 1
             if (guessesLeft < 1) {
                 alert("You lose!");
-                losses++;
                 guessesLeft = 10;
-
+                guessesSoFar = 0;
+                losses++;
             }
         }
 
-        
         // Rewrites the html with updated values
         document.getElementById('wins').innerHTML = "Wins: " + wins;
         document.getElementById('losses').innerHTML = "Losses: " + losses;
         document.getElementById('guessesLeft').innerHTML = "Guesses Left: " + guessesLeft;
-        // document.getElementById('guessesSoFar').innerHTML = "Guesses So Far: " + guessesSoFar;
+        document.getElementById('guessesSoFar').innerHTML = "Guesses So Far: " + guessesSoFar;
     }
 };
 computerChoice();
