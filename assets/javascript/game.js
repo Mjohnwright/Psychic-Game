9d7095b1032ec0ab6e44ff553e5c8e9005d0b78e
Mjohnwright <34377327+Mjohnwright@users.noreply.github.com>
2018-02-15 23:19:48 -0500

    working on getting JS correct

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,65 +1,99 @@
 
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+//Values are set
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-// Variables for tracking our wins, losses and ties. They begin at 0.
 
+wins = 0;
+losses = 0;
+guessesLeft = 10;
+let guessesSoFar = 0;
 
+//function: when user preses computer key the value is logges as 'userGuess'
+document.onkeypress = function(event) {
+    // creates  and stores a value for user keystroke
+    var userGuess = event.key;
+    guessesSoFar++;
+    guessesLeft--;
 
+    console.log("computerChoice " + computerChoice);
+    console.log("userGuess      " + userGuess);
+    console.log("guessesSoFar   " + guessesSoFar);
+    console.log("guessesLeft    " + guessesLeft);
+    console.log("losses         " + losses);
+    console.log("wins           " + wins);
+    console.log("               USer declare letter- Stage 1");
 
-//Computer guess letter and gets stored
+    //Computer picks a letter and gets stored as computerChoice
+    var computerChoice = letters[Math.floor(Math.random() * letters.length)];
 
+    console.log("computerChoice " + computerChoice);
+    console.log("userGuess      " + userGuess);
+    console.log("guessesSoFar   " + guessesSoFar);
+    console.log("guessesLeft    " + guessesLeft);
+    console.log("losses         " + losses);
+    console.log("wins           " + wins);
+    console.log("               COMP declares letter - Stage 2");
 
-var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
-                        console.log(computerChoice);
-
-document.onkeypress = function (event) {
-
-    var wins = 1;
-    var losses = 0;
-    var guessesLeft = 9;
-    var guessesSoFar = 0;
-
-    var userGuess = event.key; // creates a value for user keystroke 
-                    console.log(userGuess); //logs is so it is visible in CL
-    
+    // if the user and the computer choose the same letter: wins adds 1, alert is launched
     if (userGuess === computerChoice) {
         wins++;
         guessesLeft = 9;
         guessesSoFar = 0;
         alert("You won!!!");
-        // document.getElementById('wins').innerHTML = "Wins: " + x;
+
+        console.log("computerChoice " + computerChoice);
+        console.log("userGuess      " + userGuess);
+        console.log("guessesSoFar   " + guessesSoFar);
+        console.log("guessesLeft    " + guessesLeft);
+        console.log("losses         " + losses);
+        console.log("wins           " + wins);
+        console.log("               LAST ENTRY WIN - Stage 3");
+
+
+        // if userGuess and computerChoice are different: guessesLeft decreases 1, guessesSoFar increases 1
+    // } else {
+    //     guessesLeft = 10;
+
+    //     console.log("computerChoice " + computerChoice);
+    //     console.log("userGuess      " + userGuess);
+    //     console.log("guessesLeft    " + guessesLeft);
+    //     console.log("losses         " + losses);
+    //     console.log("wins           " + wins);
+    //     console.log("                Guesses left decreases and guesssofar increases - Stage 4");
+
+        // if guessLeft = 0: losses goes up 1, alert is launched
+    } if (guessesLeft = 0) {
+        losses++;
+        alert("You lose!");
+        // guessesLeft = 10;
+        // guessesSoFar = 0;
         
-        // if userGuess and computerChoice are = then Wins go up by 1
-                    // console.log(wins);
-    } else {
-
-        guessesLeft--; //if the letters do no match then guessesLeft lessen by 1
-                    // console.log(guessesLeft);
-        guessesSoFar++;
-                        console.log(guessesSoFar);
+        console.log("computerChoice " + computerChoice);
+        console.log("userGuess      " + userGuess);
+        console.log("guessesSoFar   " + guessesSoFar);
+        console.log("guessesLeft    " + guessesLeft);
+        console.log("losses         " + losses);
+        console.log("wins           " + wins);
+        console.log("               LOSE - Stage 5");
     }
-    if (guessesLeft = 0) {
-        losses++; // if guessesLeft = 0 then Loss = 0
-    }
+        // Rewrites the html with updated values
+        document.getElementById('wins').innerHTML = "Wins: " + wins;
+        document.getElementById('losses').innerHTML = "Losses: " + losses;
+        document.getElementById('guessesLeft').innerHTML = "Guesses Left: " + guessesLeft;
+        document.getElementById('guessesSoFar').innerHTML = "Guesses So Far: " + guessesSoFar;
 
-    // else {
-    //     alert("Only lowercase a-z allowed.");
-    // }
+        console.log("computerChoice " + computerChoice);
+        console.log("userGuess      " + userGuess); 
+        console.log("guessesSoFar   " + guessesSoFar);
+        console.log("guessesLeft    " + guessesLeft);
+        console.log("losses         " + losses);
+        console.log("wins           " + wins);
+        console.log("               INNER HTML - Last Stage 6");
+    
+    };
 
-    // var i = 10; i > guessesLeft; i--;
-    // Logs the animal at index position i to the console. This code is executed each we go through the loop.
-                        // console.log(i);
 
-    document.getElementById('wins').innerHTML = "Wins: " + wins;
-    document.getElementById('losses').innerHTML = "Losses: " + losses;
-    document.getElementById('guessesLeft').innerHTML = "Guesses Left: " + guessesLeft;
-    document.getElementById('guessesSoFar').innerHTML = "Guesses So Far: " + guessesSoFar;
 
-  
-
-};
-
-   
 
 
 
