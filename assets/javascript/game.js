bd1a15604b5dec67e64ff8acd1d383dc04f3db7b
Mjohnwright <34377327+Mjohnwright@users.noreply.github.com>
2018-02-16 22:53:07 -0500

    simplified the code logic in js but still not solved

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,51 +1,60 @@
 
 //Values are set
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+lettersIndex = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
 wins = 0;
 losses = 0;
-guessesLeft = 10;
-guessesSoFar = 0;
+guessesLeft = 9;
+guessesSoFar = []; // this is an array value
+
+
 
 //Computer picks a letter and gets stored as computerChoice
-var computerChoice = function () {
+var computerChoice = function () { // ELIMINATING THE FUNCTION HAD NO EFFECT ON PROGRAM WORKABILITY
 
-    // computer chooses a letter and it is assigned a variable name
-    var computerCurrentChoice = letters[Math.floor(Math.random() * letters.length)];
-    console.log("current Choice " + computerCurrentChoice);
+
 
     // when User presses key it launches a function that stores the letter value
     document.onkeypress = function (event) {
+
         var userGuess = event.key;
         console.log("userGuess      " + userGuess);
+        
+        // computer chooses a letter and it is assigned a variable name
+        var computerCurrentChoice = lettersIndex[Math.floor(Math.random() * lettersIndex.length)];
+        console.log(" 1 computerCurrentChoice " + computerCurrentChoice);
 
 
         // If the user and the computer choose the same letter: 
         // wins adds 1, 
-        // guessesLeft and guessesSoFar are reset to 10 and 0 respectively, 
+        // guessesLeft is reset to 9, 
+        //guessesSoFar logs the letter choice into array
         //alert is launched notifying user of win
         if (userGuess === computerCurrentChoice) {
             wins++;
-            guessesLeft = 10;
-            guessesSoFar = 0;
+            guessesLeft = 9;
+            guessesSoFar = [];
             alert("You won!!!");
-
 
             // if userGuess and computerChoice are different: 
             // guessesLeft decreases 1, 
-            // guessesSoFar increases 1
-        } else {
+            //guessesSoFar logs the letter choice into array
+        } 
+        
+        if (userGuess != computerCurrentChoice) {
             guessesLeft--;
-            guessesSoFar++;
+            guessesSoFar.push(userGuess)
 
             // if guesses left reached 0:
             //alert is launched notifying user of loss
             // guessesLeft and guessesSoFar are reset to 10 and 0 respectively,
             // losses adds 1
-            if (guessesLeft < 1) {
+        
+        if (guessesLeft < 1) {
                 alert("You lose!");
-                guessesLeft = 10;
-                guessesSoFar = 0;
+                guessesLeft = 9;
+                guessesSoFar = [];
                 losses++;
             }
         }
@@ -55,9 +64,15 @@ var computerChoice = function () {
         document.getElementById('losses').innerHTML = "Losses: " + losses;
         document.getElementById('guessesLeft').innerHTML = "Guesses Left: " + guessesLeft;
         document.getElementById('guessesSoFar').innerHTML = "Guesses So Far: " + guessesSoFar;
+
+        console.log(" 2 computerCurrentChoice " + computerCurrentChoice);
     }
+
 };
-computerChoice();
+
+
+ computerChoice(); // ELIMINATING THE FUNCTION HAD NO EFFECT ON PROGRAM WORKABILITY
+
 
 
 
