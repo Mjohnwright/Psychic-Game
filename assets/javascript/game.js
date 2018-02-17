64e4fef92bf1f354817592a190af831eb2691a42
Mjohnwright <34377327+Mjohnwright@users.noreply.github.com>
2018-02-17 13:48:04 -0500

    Code is perfect.  Runs smooth!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,58 +5,44 @@ lettersIndex = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
 
 wins = 0;
 losses = 0;
-guessesLeft = 9;
 guessesSoFar = []; // this is an array value
+guessesLeft = 10;
 
+//function is created to run a loop
+var runLoop = function () { // ELIMINATING THE FUNCTION HAD NO EFFECT ON PROGRAM WORKABILITY
 
+    // computer chooses a letter and it is assigned a variable name
+    var computerCurrentChoice = lettersIndex[Math.floor(Math.random() * lettersIndex.length)];
+    guessesLeft = 10;
 
-//Computer picks a letter and gets stored as computerChoice
-var computerChoice = function () { // ELIMINATING THE FUNCTION HAD NO EFFECT ON PROGRAM WORKABILITY
+    // when User presses key it launches a function to execute process
+    document.onkeyup = function (event) {
 
-
-
-    // when User presses key it launches a function that stores the letter value
-    document.onkeypress = function (event) {
-
+        //when user preses letter key it is stored as var userGuess
         var userGuess = event.key;
-        console.log("userGuess      " + userGuess);
-        
-        // computer chooses a letter and it is assigned a variable name
-        var computerCurrentChoice = lettersIndex[Math.floor(Math.random() * lettersIndex.length)];
-        console.log(" 1 computerCurrentChoice " + computerCurrentChoice);
 
-
-        // If the user and the computer choose the same letter: 
-        // wins adds 1, 
-        // guessesLeft is reset to 9, 
-        //guessesSoFar logs the letter choice into array
-        //alert is launched notifying user of win
+        // If the user and the computer choose the same letter:         
         if (userGuess === computerCurrentChoice) {
-            wins++;
-            guessesLeft = 9;
-            guessesSoFar = [];
-            alert("You won!!!");
+            wins++;  // wins adds 1,
+            alert("You won!!!");//alert is launched notifying user of win
+            computerCurrentChoice = lettersIndex[Math.floor(Math.random() * lettersIndex.length)];
+            guessesLeft = 10; // resets to 10
+            guessesSoFar = []; // guessesSoFar are reset to 'blank'
+        }
 
-            // if userGuess and computerChoice are different: 
-            // guessesLeft decreases 1, 
-            //guessesSoFar logs the letter choice into array
-        } 
-        
-        if (userGuess != computerCurrentChoice) {
-            guessesLeft--;
-            guessesSoFar.push(userGuess)
+        // if userGuess and computerChoice are different: 
+        else {
+            guessesLeft--; // guessesLeft decreases 1,
+            guessesSoFar.push(userGuess);  //guessesSoFar is 'pushed' and logs the letter choice into array
+        }
 
-            // if guesses left reached 0:
-            //alert is launched notifying user of loss
-            // guessesLeft and guessesSoFar are reset to 10 and 0 respectively,
-            // losses adds 1
-        
+        // if guesses left reached 0:
         if (guessesLeft < 1) {
-                alert("You lose!");
-                guessesLeft = 9;
-                guessesSoFar = [];
-                losses++;
-            }
+            alert("You lose!");//alert is launched notifying user of loss
+            guessesSoFar = []; // guessesSoFar are reset to 'blank'
+            losses++;  // losses adds 1
+            computerCurrentChoice = lettersIndex[Math.floor(Math.random() * lettersIndex.length)];
+            guessesLeft = 10; // resets to 10
         }
 
         // Rewrites the html with updated values
@@ -64,15 +50,10 @@ var computerChoice = function () { // ELIMINATING THE FUNCTION HAD NO EFFECT ON 
         document.getElementById('losses').innerHTML = "Losses: " + losses;
         document.getElementById('guessesLeft').innerHTML = "Guesses Left: " + guessesLeft;
         document.getElementById('guessesSoFar').innerHTML = "Guesses So Far: " + guessesSoFar;
-
-        console.log(" 2 computerCurrentChoice " + computerCurrentChoice);
     }
-
 };
 
-
- computerChoice(); // ELIMINATING THE FUNCTION HAD NO EFFECT ON PROGRAM WORKABILITY
-
+runLoop(); //  Calls the function
 
 
 
